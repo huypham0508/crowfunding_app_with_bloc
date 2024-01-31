f148b01d3d621d82475428dbd0356c6d7d321075
Mạnh Huy <phammanhhuy1107@gmail.com>
2024-01-31 15:00:43 +0700

    [add][add firebase]

--- lib/app/modules/lo_to/bloc/lo_to_bloc.dart
+++ lib/app/modules/lo_to/bloc/lo_to_bloc.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 
 import 'package:bloc/bloc.dart';
 import 'package:crowfunding_app_with_bloc/app/data/local_data_source.dart';
+import 'package:crowfunding_app_with_bloc/app/modules/lo_to/firebase/firebase_data.dart';
 
 part 'lo_to_events.dart';
 part 'lo_to_state.dart';
@@ -11,7 +12,7 @@ part 'lo_to_state.dart';
 class LoToBloc extends Bloc<LotoEvent, LotoState> {
   LoToBloc({
     required this.localDataSource,
-    // required this.firebaseDatabase,
+    required this.firebaseDatabase,
   }) : super(lotoInitialState) {
     on<InitialEvent>(_initial);
     on<ListenCount>(_listenerCount);
@@ -20,7 +21,7 @@ class LoToBloc extends Bloc<LotoEvent, LotoState> {
   }
 
   final LocalDataSource localDataSource;
-  // final LotoFirebaseDatabase firebaseDatabase;
+  final LotoFirebaseDatabase firebaseDatabase;
 
   void _initial(InitialEvent event, Emitter<LotoState> emit) async {
     _listenState();
@@ -50,33 +51,33 @@ class LoToBloc extends Bloc<LotoEvent, LotoState> {
     emit(state.copyWith(status: LotoStatus.loading));
     localDataSource.saveUserName(event.userName);
     await _backDialog(emit);
-    // await firebaseDatabase.updateUserName(event.userName, state.listUsers).then(
-    //   (value) {
-    //     emit(
-    //       state.copyWith(
-    //         userName: event.userName,
-    //         status: LotoStatus.userNameSuccess,
-    //       ),
-    //     );
-    //   },
-    // ).catchError((onError) {
-    //   emit(state.copyWith(status: LotoStatus.userNameFailure));
-    // });
+    await firebaseDatabase.updateUserName(event.userName, state.listUsers).then(
+      (value) {
+        emit(
+          state.copyWith(
+            userName: event.userName,
+            status: LotoStatus.userNameSuccess,
+          ),
+        );
+      },
+    ).catchError((onError) {
+      emit(state.copyWith(status: LotoStatus.userNameFailure));
+    });
   }
 
 // Functions only used in bloc
 
   void _listenState() async {
-    // await firebaseDatabase.listenData(
-    //   (event) async {
-    //     add(ListenCount(count: event.snapshot.value.toString()));
-    //   },
-    // );
-    // await firebaseDatabase.listenUsers(
-    //   (event) async {
-    //     add(ListenUsers(users: event.snapshot.value.toString()));
-    //   },
-    // );
+    await firebaseDatabase.listenData(
+      (event) async {
+        add(ListenCount(count: event.snapshot.value.toString()));
+      },
+    );
+    await firebaseDatabase.listenUsers(
+      (event) async {
+        add(ListenUsers(users: event.snapshot.value.toString()));
+      },
+    );
   }
 
   Future _backDialog(Emitter<LotoState> emit) async {
--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,103 @@
+import 'package:crowfunding_app_with_bloc/app/constants/firebase_database.dart';
+import 'package:crowfunding_app_with_bloc/app/constants/graph_ql_string.dart';
+import 'package:crowfunding_app_with_bloc/app/data/firebase/firebase_api.dart';
+import 'package:crowfunding_app_with_bloc/app/data/local_data_source.dart';
+import 'package:crowfunding_app_with_bloc/app/data/provider/graph_QL.dart';
+import 'package:crowfunding_app_with_bloc/app/data/provider/rest.dart';
+import 'package:crowfunding_app_with_bloc/app/data/repository/api_service_repository.dart';
+import 'package:crowfunding_app_with_bloc/app/global_bloc/auth/auth_bloc.dart';
+import 'package:crowfunding_app_with_bloc/app/modules/lo_to/bloc/lo_to_bloc.dart';
+import 'package:crowfunding_app_with_bloc/app/modules/lo_to/firebase/firebase_data.dart';
 import 'package:crowfunding_app_with_bloc/app/routes/app_pages.dart';
+import 'package:crowfunding_app_with_bloc/firebase_options.dart';
+import 'package:dio/dio.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
-  // await FirebaseApi().initNotification();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseApi().initNotification();
 
   /// Use preferences like expected.
   await SharedPreferences.getInstance();
-  // final sf = await SharedPreferences.getInstance();
-  runApp(const MainApp(
-      // sharedPreferences: sf,
-      ));
+  final sf = await SharedPreferences.getInstance();
+  runApp(MainApp(
+    sharedPreferences: sf,
+  ));
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({
-    super.key,
-  });
-  // final SharedPreferences sharedPreferences;
+  MainApp({super.key, required this.sharedPreferences});
+  final SharedPreferences sharedPreferences;
+
+  final GraphQLService graphQLService = GraphQLService(GraphQLClient(
+    link: ConfigGraphQl.httpLink,
+    cache: GraphQLCache(),
+  ));
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: AppRouter.returnRouter(navigatorKey),
+    return MultiRepositoryProvider(
+      providers: [
+        //Create a LocalDataSource
+        RepositoryProvider(
+          create: (context) => LocalDataSource(sharedPreferences),
+        ),
+        //Create a RestAPIClientService
+        RepositoryProvider(
+          create: (context) => ApiServiceRepository(
+            RestAPIClient(
+              httpClient: Dio(),
+            ),
+          ),
+        ),
+        //Create a auth graphQLService
+        RepositoryProvider(
+          create: (context) => AuthRepository(
+            graphQLClient: graphQLService,
+            localDataSource: LocalDataSource(sharedPreferences),
+          ),
+        ),
+        //Create a firebaseDatabase
+        RepositoryProvider(
+          create: (context) => LotoFirebaseDatabase(
+            refData: FirebaseDatabase.instance.ref(
+              FirebaseDatabaseString.count,
+            ),
+            refUsers: FirebaseDatabase.instance.ref(
+              FirebaseDatabaseString.users,
+            ),
+          ),
+        ),
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => AuthBloc(
+              authRepository: context.read<AuthRepository>(),
+            ),
+          ),
+          BlocProvider(
+            create: (context) => LoToBloc(
+              localDataSource: context.read<LocalDataSource>(),
+              firebaseDatabase: context.read<LotoFirebaseDatabase>(),
+            ),
+          ),
+        ],
+        child: MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          routerConfig: AppRouter.returnRouter(navigatorKey),
+        ),
+      ),
     );
   }
 }
